33835cb137a86db671c63e4bb8b99d1f77c0bbf6
Sahil Agarwal <sahildagarwal@gmail.com>
2018-10-15 22:05:55 -0700

    Upped to v1.6.0, updated for 2018-2019 season

---
+++ Changelog.md
@@ -0,0 +1,2 @@
+# 10-14-2018
+* Updated for the 2018-2019 basketball season
--- content_script.js
+++ content_script.js
@@ -1,69 +1,69 @@
-var teamMap = {
-    "Atl":"Atlanta Hawks",
-    "Bkn":"Brooklyn Nets",
-    "Bos":"Boston Celtics",
-    "Cha":"Charlotte Hornets",
-    "Chi":"Chicago Bulls",
-    "Cle":"Cleveland Cavaliers",
-    "Dal":"Dallas Mavericks",
-    "Den":"Denver Nuggets",
-    "Det":"Detroit Pistons",
-    "GS":"Golden State Warriors",
-    "Hou":"Houston Rockets",
-    "Ind":"Indiana Pacers",
-    "LAC":"Los Angeles Clippers",
-    "LAL":"Los Angeles Lakers",
-    "Mem":"Memphis Grizzlies",
-    "Mia":"Miami Heat",
-    "Mil":"Milwaukee Bucks",
-    "Min":"Minnesota Timberwolves",
-    "NO":"New Orleans Pelicans",
-    "NY":"New York Knicks",
-    "OKC":"Oklahoma City Thunder",
-    "Orl":"Orlando Magic",
-    "Phi":"Philadelphia 76ers",
-    "Pho":"Phoenix Suns",
-    "Por":"Portland Trail Blazers",
-    "SA":"San Antonio Spurs",
-    "Sac":"Sacramento Kings",
-    "Tor":"Toronto Raptors",
-    "Uta":"Utah Jazz",
-    "Was":"Washington Wizards"
+// schedule shows games per week for each team, first week starting 10/14/2018
+var teamMap1819 = {
+    "Atl":"ATL",
+    "Bkn":"BKN",
+    "Bos":"BOS",
+    "Cha":"CHA",
+    "Chi":"CHI",
+    "Cle":"CLE",
+    "Dal":"DAL",
+    "Den":"DEN",
+    "Det":"DET",
+    "GS":"GSW",
+    "Hou":"HOU",
+    "Ind":"IND",
+    "LAC":"LAC",
+    "LAL":"LAL",
+    "Mem":"MEM",
+    "Mia":"MIA",
+    "Mil":"MIL",
+    "Min":"MIN",
+    "NO":"NOR",
+    "NY":"NYK",
+    "OKC":"OKC",
+    "Orl":"ORL",
+    "Phi":"PHI",
+    "Pho":"PHO",
+    "Por":"POR",
+    "SA":"SAC",
+    "Sac":"SAS",
+    "Tor":"TOR",
+    "Uta":"UTA",
+    "Was":"WAS",
 };
 
-// schedule shows games per week for each team, first week starting 10/16/2017
-var teamSchedule = {
-	"Atlanta Hawks": [3, 4, 3, 3, 3, 4, 2, 4, 3, 4, 3, 3, 3, 3, 4, 4, 4, 2, 1, 4, 3, 3, 4, 3, 4, 1],
-	"Boston Celtics": [3, 3, 4, 4, 3, 4, 3, 4, 4, 4, 4, 3, 1, 3, 3, 4, 4, 1, 2, 3, 3, 3, 3, 3, 4, 2],
-	"Brooklyn Nets": [3, 4, 2, 4, 3, 3, 3, 3, 4, 3, 4, 3, 4, 4, 3, 4, 3, 2, 1, 4, 3, 3, 4, 3, 3, 2],
-	"Charlotte Hornets": [2, 4, 4, 2, 3, 4, 2, 4, 4, 4, 3, 2, 3, 3, 4, 4, 4, 1, 3, 4, 3, 3, 4, 4, 3, 1],
-	"Chicago Bulls": [2, 3, 3, 3, 3, 4, 3, 4, 3, 4, 4, 4, 3, 3, 4, 2, 3, 2, 2, 3, 4, 3, 4, 4, 3, 2],
-	"Cleveland Cavaliers": [3, 4, 3, 3, 3, 3, 4, 4, 4, 2, 3, 3, 3, 3, 3, 3, 4, 1, 3, 3, 4, 3, 4, 4, 3, 2],
-	"Dallas Mavericks": [3, 4, 4, 3, 3, 3, 3, 4, 3, 4, 4, 3, 3, 2, 4, 3, 4, 1, 2, 4, 3, 3, 3, 4, 4, 1],
-	"Denver Nuggets": [2, 4, 4, 3, 3, 3, 3, 4, 3, 4, 3, 3, 4, 3, 3, 4, 3, 2, 2, 3, 4, 3, 3, 4, 3, 2],
-	"Detroit Pistons": [3, 4, 3, 3, 3, 2, 4, 4, 4, 2, 3, 3, 3, 4, 3, 3, 4, 2, 2, 4, 3, 3, 4, 4, 3, 2],
-	"Golden State Warriors": [3, 4, 3, 3, 4, 3, 4, 3, 2, 4, 4, 3, 4, 3, 3, 3, 3, 2, 2, 3, 4, 3, 3, 4, 4, 1],
-	"Houston Rockets": [3, 4, 4, 3, 3, 2, 3, 2, 4, 3, 4, 3, 3, 3, 4, 3, 4, 2, 2, 3, 4, 4, 4, 3, 3, 2],
-	"Indiana Pacers": [3, 3, 4, 4, 3, 3, 3, 4, 3, 3, 4, 2, 4, 4, 3, 4, 4, 1, 1, 4, 4, 3, 3, 3, 4, 1],
-	"Los Angeles Clippers": [2, 3, 4, 3, 3, 3, 4, 2, 4, 4, 3, 3, 4, 3, 4, 2, 3, 2, 2, 4, 3, 4, 4, 4, 3, 2],
-	"Los Angeles Lakers": [3, 3, 4, 3, 4, 2, 4, 2, 2, 4, 4, 4, 3, 4, 3, 3, 3, 2, 2, 3, 4, 3, 3, 4, 4, 2],
-	"Memphis Grizzlies": [2, 4, 4, 2, 3, 4, 3, 4, 4, 3, 4, 2, 2, 4, 3, 4, 3, 1, 2, 4, 4, 3, 4, 4, 3, 2],
-	"Miami Heat": [2, 3, 4, 4, 3, 3, 4, 2, 4, 4, 3, 3, 3, 4, 3, 4, 3, 2, 2, 3, 4, 3, 4, 3, 3, 2],
-	"Milwaukee Bucks": [3, 3, 3, 3, 3, 3, 3, 4, 3, 3, 3, 4, 4, 3, 3, 4, 3, 2, 2, 4, 3, 3, 4, 4, 3, 2],
-	"Minnesota Timberwolves": [3, 3, 4, 2, 4, 4, 4, 3, 3, 3, 4, 4, 4, 3, 4, 4, 3, 2, 2, 3, 2, 3, 3, 4, 2, 2],
-	"New Orleans Pelicans": [3, 3, 4, 3, 3, 4, 3, 4, 3, 3, 3, 2, 4, 3, 4, 3, 4, 2, 2, 3, 4, 4, 3, 3, 3, 2],
-	"New York Knicks": [2, 3, 4, 3, 3, 4, 3, 4, 3, 3, 4, 4, 3, 4, 3, 4, 3, 2, 2, 3, 3, 3, 4, 3, 3, 2],
-	"Oklahoma City Thunder": [3, 3, 3, 4, 2, 4, 3, 3, 4, 4, 4, 3, 3, 3, 4, 4, 3, 2, 2, 4, 3, 4, 3, 3, 2, 2],
-	"Orlando Magic": [3, 3, 4, 3, 3, 4, 4, 4, 3, 3, 3, 3, 3, 3, 2, 3, 4, 2, 2, 4, 4, 3, 3, 3, 4, 2],
-	"Philadelphia 76ers": [3, 3, 3, 3, 3, 3, 4, 4, 2, 4, 4, 2, 1, 3, 4, 4, 3, 2, 3, 4, 3, 3, 4, 4, 4, 2],
-	"Phoenix Suns": [3, 3, 4, 4, 4, 3, 3, 4, 3, 4, 3, 4, 2, 2, 4, 4, 3, 2, 2, 4, 3, 3, 3, 4, 3, 1],
-	"Portland Trail Blazers": [3, 3, 4, 2, 4, 4, 3, 2, 4, 4, 2, 4, 4, 3, 3, 4, 4, 1, 2, 3, 3, 4, 3, 4, 3, 2],
-	"Sacramento Kings": [3, 3, 3, 3, 4, 3, 4, 3, 3, 3, 4, 2, 4, 3, 4, 3, 3, 2, 2, 5, 3, 4, 3, 4, 2, 2],
-	"San Antonio Spurs": [2, 4, 4, 3, 3, 3, 4, 4, 3, 4, 3, 4, 3, 4, 4, 3, 2, 2, 2, 2, 3, 4, 4, 3, 3, 2],
-	"Toronto Raptors": [2, 3, 4, 3, 4, 3, 2, 3, 4, 3, 3, 3, 4, 4, 3, 4, 3, 2, 1, 4, 4, 4, 4, 2, 4, 2],
-	"Utah Jazz": [3, 3, 4, 3, 4, 3, 3, 4, 3, 4, 3, 3, 2, 4, 3, 3, 4, 2, 2, 3, 4, 3, 4, 3, 3, 2],
-	"Washington Wizards": [2, 4, 3, 3, 4, 3, 3, 4, 4, 3, 4, 3, 3, 3, 3, 3, 4, 1, 3, 4, 3, 3, 3, 4, 3, 2]
-};
+var teamSchedule1819 = {
+    "ATL": [3,2,4,4,3,4,3,3,3,3,3,4,4,2,3,4,4,2,2,4,4,3,3,3,4,1],
+    "BKN": [3,3,4,3,4,4,3,4,3,4,3,3,3,3,3,4,3,2,2,4,3,4,2,3,4,1],
+    "BOS": [3,3,3,4,3,4,3,2,4,3,3,3,4,4,3,4,3,2,2,4,3,3,4,3,4,1],
+    "CHA": [3,4,3,3,2,4,4,3,3,3,3,4,3,4,2,4,3,2,2,4,3,3,4,3,4,2],
+    "CHI": [2,4,4,3,4,3,4,3,3,4,3,3,3,3,4,3,3,2,2,4,4,3,3,3,3,2],
+    "CLE": [3,3,3,3,2,4,4,4,4,4,3,3,4,3,4,2,3,2,2,4,2,4,4,3,4,1],
+    "DAL": [2,4,3,3,3,3,3,3,4,4,3,4,4,2,4,3,3,2,2,3,4,3,4,3,4,2],
+    "DEN": [3,2,4,4,3,4,2,4,3,2,3,3,5,3,3,4,3,2,2,3,3,3,4,4,4,2],
+    "DET": [2,3,3,4,1,4,3,4,3,4,3,3,4,4,3,3,3,2,2,4,3,4,4,3,4,2],
+    "GSW": [3,4,3,3,5,3,3,3,3,4,3,3,3,3,3,3,3,2,2,4,3,2,5,3,4,2],
+    "HOU": [3,2,3,4,3,3,4,3,3,4,3,3,4,3,4,3,3,2,2,4,3,4,4,3,4,1],
+    "IND": [3,3,4,4,2,3,4,3,4,4,2,3,3,4,2,4,4,2,2,4,3,3,4,3,4,1],
+    "LAC": [3,3,3,3,3,4,3,3,4,4,3,3,3,4,4,4,3,2,2,4,2,4,3,4,3,1],
+    "LAL": [2,4,4,3,3,3,4,3,4,3,4,3,4,3,3,3,3,1,2,4,3,4,3,4,4,1],
+    "MEM": [2,3,3,3,4,4,3,3,4,4,2,4,3,4,4,4,3,2,2,4,3,2,3,4,3,2],
+    "MIA": [3,2,3,4,4,3,3,3,4,3,3,3,3,3,4,3,3,2,2,4,4,3,4,3,4,2],
+    "MIL": [2,4,3,4,2,4,3,3,3,4,3,3,4,3,3,3,4,2,2,4,4,3,4,3,4,1],
+    "MIN": [3,3,4,3,4,3,3,3,3,4,3,4,3,3,4,2,3,2,2,4,4,3,3,3,4,2],
+    "NOR": [2,3,4,3,4,4,4,4,3,3,3,3,3,3,4,3,4,2,2,4,4,3,3,3,3,1],
+    "NYK": [3,3,4,4,3,4,3,4,3,3,3,2,4,1,4,4,3,3,2,3,4,3,4,2,4,2],
+    "OKC": [3,2,3,4,3,4,2,3,4,4,3,4,3,3,4,3,3,2,2,4,3,4,3,4,3,2],
+    "ORL": [3,3,3,4,4,3,3,3,3,3,3,4,4,3,4,3,4,2,2,4,3,3,2,4,4,1],
+    "PHI": [3,3,5,3,4,4,3,2,4,3,3,3,4,3,3,3,3,2,2,3,4,3,3,3,4,2],
+    "PHO": [2,4,3,3,3,4,4,3,4,4,3,4,3,4,4,2,4,1,2,3,4,3,3,3,4,1],
+    "POR": [2,3,5,3,3,4,3,3,3,4,4,3,4,3,4,1,3,2,2,4,3,3,3,4,4,2],
+    "SAC": [3,3,4,3,3,4,2,3,4,4,3,3,4,3,4,2,4,1,2,3,3,4,4,4,4,1],
+    "SAS": [2,3,4,2,4,4,4,4,3,4,3,3,4,4,3,3,4,1,2,3,3,3,4,3,4,1],
+    "TOR": [3,3,4,3,4,4,3,4,4,3,3,4,3,3,4,2,3,2,2,3,3,3,4,3,4,1],
+    "UTA": [2,4,3,3,4,4,4,3,3,4,3,3,4,3,4,3,2,1,2,3,3,4,4,3,4,2],
+    "WAS": [2,4,3,3,4,3,4,3,4,4,3,3,4,1,4,3,4,2,2,3,3,4,4,4,3,1]
+}
 
 function isAddPlayerPage() {
     var pathname = window.location.pathname;
@@ -93,10 +93,7 @@ function getDateFromURL() {
 function getWeekFromURL() {
     var search = window.location.search;
     if (search.includes("week=")) {
-        var week = search.split("week=")[1].split("&")[0] - 1;
-        if (isMatchupPage()) {
-            return week + 1;
-        };
+        var week = search.split("week=")[1].split("&")[0];    
         return week;
     }
     return -1;
@@ -107,9 +104,9 @@ function getCurrentWeek() {
         return getWeekFromURL();
     }
     var currentDate = getDateFromURL();
-    var startDate = new Date("10/16/2017");
+    var startDate = new Date("10/14/2018");
     var day = 1000 * 60 * 60 * 24;
-    var week = Math.floor((currentDate - startDate) / day / 7);
+    var week = Math.floor((currentDate - startDate) / day / 7) + 1;
     if (isAddPlayerPage()) {
         return week + 1;
     };
@@ -178,8 +175,7 @@ function addHeader(liveScoring, week) {
                 }
             }
         }
-    }
-            
+    }            
 }
 
 function addHeaderNode(elements, week) {
@@ -187,9 +183,9 @@ function addHeaderNode(elements, week) {
         if (typeof elements[i] === 'object' && elements[i].innerText.includes("Player")) {
             var span = document.createElement("span");
             span.style.color = 'firebrick';
-            var text = ("\n(Showing games for week " + (week + 1) +")");
+            var text = ("\n(Showing games for week " + week +")");
             if (isAddPlayerPage()) {
-                text = ("\n(Showing games for next week - week " + (week + 1) +")");
+                text = ("\n(Showing games for next week - week " + week +")");
             };
             var node = document.createTextNode(text);
             span.appendChild(node);
@@ -209,7 +205,7 @@ function getDash() {
 function getGamesLeftSpan(gamesLeft) {    
     var span = document.createElement("span");
     span.style.color = 'firebrick';
-    var node = document.createTextNode("(" + gamesLeft + ")");
+    var node = document.createTextNode("(" + gamesLeft + ") ");
     span.appendChild(node);
     return span;
 }
@@ -218,24 +214,24 @@ function main() {
     var liveScoring = getLiveScoring();
     var week = getCurrentWeek();
     if (week > 25 || week < 0) {
-        console.log("No data for week " + (week + 1));
+        console.log("No data for week " + week);
         return;
     }
-    console.log("Showing games for week " + (week + 1));
+    console.log("Showing games for week " + week);
     var players = getPlayers(liveScoring);
+
     for (var i in players) {
         var player = players[i];    
         if (typeof player === 'object' && !player.innerText.includes("Empty")) {
             var teamPosition = getTeamPosition(liveScoring, player);
             var team = teamPosition.split("-")[0];
             team = team.substring(0, team.length - 1);
-            var gamesLeft = teamSchedule[teamMap[team]][week];            
+            var gamesLeft = teamSchedule1819[teamMap1819[team]][week - 1];            
             if (liveScoring) {
                 player.appendChild(getDash());
                 player.appendChild(getGamesLeftSpan(gamesLeft));
             } else {
-                player.getElementsByClassName("Fz-xxs")[0].appendChild(getDash());
-                player.getElementsByClassName("Fz-xxs")[0].appendChild(getGamesLeftSpan(gamesLeft));
+                player.prepend(getGamesLeftSpan(gamesLeft));
             }        
         }
     }
@@ -258,3 +254,70 @@ function main() {
 
 console.log("Thanks for using fbb extender!");
 setTimeout(function() { main(); }, getTimer());
+
+// schedule shows games per week for each team, first week starting 10/16/2017
+// var teamMap1718 = {
+//     "Atl":"Atlanta Hawks",
+//     "Bkn":"Brooklyn Nets",
+//     "Bos":"Boston Celtics",
+//     "Cha":"Charlotte Hornets",
+//     "Chi":"Chicago Bulls",
+//     "Cle":"Cleveland Cavaliers",
+//     "Dal":"Dallas Mavericks",
+//     "Den":"Denver Nuggets",
+//     "Det":"Detroit Pistons",
+//     "GS":"Golden State Warriors",
+//     "Hou":"Houston Rockets",
+//     "Ind":"Indiana Pacers",
+//     "LAC":"Los Angeles Clippers",
+//     "LAL":"Los Angeles Lakers",
+//     "Mem":"Memphis Grizzlies",
+//     "Mia":"Miami Heat",
+//     "Mil":"Milwaukee Bucks",
+//     "Min":"Minnesota Timberwolves",
+//     "NO":"New Orleans Pelicans",
+//     "NY":"New York Knicks",
+//     "OKC":"Oklahoma City Thunder",
+//     "Orl":"Orlando Magic",
+//     "Phi":"Philadelphia 76ers",
+//     "Pho":"Phoenix Suns",
+//     "Por":"Portland Trail Blazers",
+//     "SA":"San Antonio Spurs",
+//     "Sac":"Sacramento Kings",
+//     "Tor":"Toronto Raptors",
+//     "Uta":"Utah Jazz",
+//     "Was":"Washington Wizards"
+// };
+
+// var teamSchedule1718 = {
+// 	"Atlanta Hawks": [3, 4, 3, 3, 3, 4, 2, 4, 3, 4, 3, 3, 3, 3, 4, 4, 4, 2, 1, 4, 3, 3, 4, 3, 4, 1],
+// 	"Boston Celtics": [3, 3, 4, 4, 3, 4, 3, 4, 4, 4, 4, 3, 1, 3, 3, 4, 4, 1, 2, 3, 3, 3, 3, 3, 4, 2],
+// 	"Brooklyn Nets": [3, 4, 2, 4, 3, 3, 3, 3, 4, 3, 4, 3, 4, 4, 3, 4, 3, 2, 1, 4, 3, 3, 4, 3, 3, 2],
+// 	"Charlotte Hornets": [2, 4, 4, 2, 3, 4, 2, 4, 4, 4, 3, 2, 3, 3, 4, 4, 4, 1, 3, 4, 3, 3, 4, 4, 3, 1],
+// 	"Chicago Bulls": [2, 3, 3, 3, 3, 4, 3, 4, 3, 4, 4, 4, 3, 3, 4, 2, 3, 2, 2, 3, 4, 3, 4, 4, 3, 2],
+// 	"Cleveland Cavaliers": [3, 4, 3, 3, 3, 3, 4, 4, 4, 2, 3, 3, 3, 3, 3, 3, 4, 1, 3, 3, 4, 3, 4, 4, 3, 2],
+// 	"Dallas Mavericks": [3, 4, 4, 3, 3, 3, 3, 4, 3, 4, 4, 3, 3, 2, 4, 3, 4, 1, 2, 4, 3, 3, 3, 4, 4, 1],
+// 	"Denver Nuggets": [2, 4, 4, 3, 3, 3, 3, 4, 3, 4, 3, 3, 4, 3, 3, 4, 3, 2, 2, 3, 4, 3, 3, 4, 3, 2],
+// 	"Detroit Pistons": [3, 4, 3, 3, 3, 2, 4, 4, 4, 2, 3, 3, 3, 4, 3, 3, 4, 2, 2, 4, 3, 3, 4, 4, 3, 2],
+// 	"Golden State Warriors": [3, 4, 3, 3, 4, 3, 4, 3, 2, 4, 4, 3, 4, 3, 3, 3, 3, 2, 2, 3, 4, 3, 3, 4, 4, 1],
+// 	"Houston Rockets": [3, 4, 4, 3, 3, 2, 3, 2, 4, 3, 4, 3, 3, 3, 4, 3, 4, 2, 2, 3, 4, 4, 4, 3, 3, 2],
+// 	"Indiana Pacers": [3, 3, 4, 4, 3, 3, 3, 4, 3, 3, 4, 2, 4, 4, 3, 4, 4, 1, 1, 4, 4, 3, 3, 3, 4, 1],
+// 	"Los Angeles Clippers": [2, 3, 4, 3, 3, 3, 4, 2, 4, 4, 3, 3, 4, 3, 4, 2, 3, 2, 2, 4, 3, 4, 4, 4, 3, 2],
+// 	"Los Angeles Lakers": [3, 3, 4, 3, 4, 2, 4, 2, 2, 4, 4, 4, 3, 4, 3, 3, 3, 2, 2, 3, 4, 3, 3, 4, 4, 2],
+// 	"Memphis Grizzlies": [2, 4, 4, 2, 3, 4, 3, 4, 4, 3, 4, 2, 2, 4, 3, 4, 3, 1, 2, 4, 4, 3, 4, 4, 3, 2],
+// 	"Miami Heat": [2, 3, 4, 4, 3, 3, 4, 2, 4, 4, 3, 3, 3, 4, 3, 4, 3, 2, 2, 3, 4, 3, 4, 3, 3, 2],
+// 	"Milwaukee Bucks": [3, 3, 3, 3, 3, 3, 3, 4, 3, 3, 3, 4, 4, 3, 3, 4, 3, 2, 2, 4, 3, 3, 4, 4, 3, 2],
+// 	"Minnesota Timberwolves": [3, 3, 4, 2, 4, 4, 4, 3, 3, 3, 4, 4, 4, 3, 4, 4, 3, 2, 2, 3, 2, 3, 3, 4, 2, 2],
+// 	"New Orleans Pelicans": [3, 3, 4, 3, 3, 4, 3, 4, 3, 3, 3, 2, 4, 3, 4, 3, 4, 2, 2, 3, 4, 4, 3, 3, 3, 2],
+// 	"New York Knicks": [2, 3, 4, 3, 3, 4, 3, 4, 3, 3, 4, 4, 3, 4, 3, 4, 3, 2, 2, 3, 3, 3, 4, 3, 3, 2],
+// 	"Oklahoma City Thunder": [3, 3, 3, 4, 2, 4, 3, 3, 4, 4, 4, 3, 3, 3, 4, 4, 3, 2, 2, 4, 3, 4, 3, 3, 2, 2],
+// 	"Orlando Magic": [3, 3, 4, 3, 3, 4, 4, 4, 3, 3, 3, 3, 3, 3, 2, 3, 4, 2, 2, 4, 4, 3, 3, 3, 4, 2],
+// 	"Philadelphia 76ers": [3, 3, 3, 3, 3, 3, 4, 4, 2, 4, 4, 2, 1, 3, 4, 4, 3, 2, 3, 4, 3, 3, 4, 4, 4, 2],
+// 	"Phoenix Suns": [3, 3, 4, 4, 4, 3, 3, 4, 3, 4, 3, 4, 2, 2, 4, 4, 3, 2, 2, 4, 3, 3, 3, 4, 3, 1],
+// 	"Portland Trail Blazers": [3, 3, 4, 2, 4, 4, 3, 2, 4, 4, 2, 4, 4, 3, 3, 4, 4, 1, 2, 3, 3, 4, 3, 4, 3, 2],
+// 	"Sacramento Kings": [3, 3, 3, 3, 4, 3, 4, 3, 3, 3, 4, 2, 4, 3, 4, 3, 3, 2, 2, 5, 3, 4, 3, 4, 2, 2],
+// 	"San Antonio Spurs": [2, 4, 4, 3, 3, 3, 4, 4, 3, 4, 3, 4, 3, 4, 4, 3, 2, 2, 2, 2, 3, 4, 4, 3, 3, 2],
+// 	"Toronto Raptors": [2, 3, 4, 3, 4, 3, 2, 3, 4, 3, 3, 3, 4, 4, 3, 4, 3, 2, 1, 4, 4, 4, 4, 2, 4, 2],
+// 	"Utah Jazz": [3, 3, 4, 3, 4, 3, 3, 4, 3, 4, 3, 3, 2, 4, 3, 3, 4, 2, 2, 3, 4, 3, 4, 3, 3, 2],
+// 	"Washington Wizards": [2, 4, 3, 3, 4, 3, 3, 4, 4, 3, 4, 3, 3, 3, 3, 3, 4, 1, 3, 4, 3, 3, 3, 4, 3, 2]
+// };
